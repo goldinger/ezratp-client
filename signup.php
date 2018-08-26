72dd25ad72ccc7a4518ea954229de48106a106d7
Amine <sghir.ma@gmail.com>
2018-08-26 22:29:59 +0200

    update

---
+++ signup.php
@@ -0,0 +1,36 @@
+<html>
+    <head>
+        <title>Sign up</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <div align="center">
+            <h2>Sign up form</h2>
+            <br><br><br>
+            <form method="POST" action="">
+                <table align="right">
+                    <tr>
+                        <td><label for="username">Pseudo :</label></td>
+                        <td><input type="text" id="username" placeholder="Chose a username" name="username" /></td>
+                    </tr>
+                    <tr>
+                        <td><label for="email">Email :</label></td>
+                        <td><input type="text" id="email" placeholder="Enter your email address" name="email" /></td>
+                    </tr>
+                    <tr>
+                        <td><label for="email2">Email confirmation :</label></td>
+                        <td><input type="text" id="email2" placeholder="Confirm your email address" name="email2" /></td>
+                    </tr>
+                    <tr>
+                        <td><label for="password">Password :</label></td>
+                        <td><input type="password" id="password" placeholder="Password" name="password" /></td>
+                    </tr>
+                    <tr>
+                        <td><label for="password2">Password confirmation :</label></td>
+                        <td><input type="password" id="password2" placeholder="Confirm your password" name="password2" /></td>
+                    </tr>
+                </table>
+            </form>
+        </div>
+    </body>
+</html>
