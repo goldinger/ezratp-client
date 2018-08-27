fe886e562e42bf875b001da80befb5d11867323e
Amine <sghir.ma@gmail.com>
2018-08-28 00:04:24 +0200

    update

--- header.php
+++ header.php
@@ -4,35 +4,32 @@ $db = new PDO('mysql:host=localhost;dbname=ezratp', 'root', 'VnCdE28u');
 
 $connected = false;
 
-if(isset($_POST['formsignin'])){
-    $emailconnect = htmlspecialchars($_POST['emailconnect']);
-    $passwordconnect = sha1($_POST['passwordconnect']);
-    if(!empty($emailconnect) AND !empty($passwordconnect)){
+if(isset($_SESSION['email']) AND isset($_SESSION['password'])){
+    if(!empty($emailconnect) AND !empty($passwordconnect)) {
         $req_user = $db->prepare("SELECT * FROM users WHERE email = ? AND password = ?");
-        $req_user->execute(array($emailconnect, $passwordconnect));
+        $req_user->execute(array($_SESSION['email'], $_SESSION['password']));
         $correct_ids = $req_user->rowCount();
-        if($correct_ids == 1){
+        if ($correct_ids == 1) {
             $user_info = $req_user->fetch();
             $connected = true;
             $_SESSION['id'] = $user_info['id'];
-            echo $user_info['username'];
             $_SESSION['username'] = $user_info['username'];
-            echo $_SESSION['username'];
             $_SESSION['email'] = $user_info['email'];
             $_SESSION['password'] = $user_info['password'];
         }
-        else {
-            $error_signin = "Wrong Ids";
-        }
-    }
-    else {
-        $error_signin = "Missing information for signup";
     }
 }
 
+
 if($connected){
 ?>
-<h3>Hello <?php echo $_SESSION['username'] ?></h3>
+    <h3>Hello <?php echo $_SESSION['username'] ?></h3>
+    <h3>Dashboard</h3>
+    <?php
+    $req_watchlist = $db->prepare("SELECT * FROM watchlist WHERE user_id = ?");
+    $req_watchlist->execute(array($_SESSION['email']));
+    echo $req_watchlist->rowCount();
+    ?>
 <?php
 }
 else {
