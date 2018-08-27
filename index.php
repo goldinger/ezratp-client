21e19f38bab48381faf4bb18873b50f075f0690d
Amine <sghir.ma@gmail.com>
2018-08-27 23:18:23 +0200

    update

--- index.php
+++ index.php
@@ -1,8 +1,31 @@
 <?php
-    session_start();
-    echo $_SESSION['id'];
-    $db = new PDO('mysql:host=localhost;dbname=ezratp', 'root', 'VnCdE28u');
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+session_start();
+$db = new PDO('mysql:host=localhost;dbname=ezratp', 'root', 'VnCdE28u');
+
+if(isset($_POST['formsignin'])){
+    $emailconnect = htmlspecialchars($_POST['emailconnect']);
+    $passwordconnect = sha1($_POST['passwordconnect']);
+    if(!empty($emailconnect) AND !empty($passwordconnect)){
+        $req_user = $db->prepare("SELECT * FROM users WHERE email = ? AND password = ?");
+        $req_user->execute(array($emailconnect, $passwordconnect));
+        $correct_ids = $req_user->rowCount();
+        if($correct_ids == 1){
+            $req_user = $req_user->fetch();
+            echo $req_user;
+            $_SESSION['id'] = $req_user['id'];
+            $_SESSION['username'] = $req_user['username'];
+            $_SESSION['email'] = $req_user['email'];
+            $_SESSION['password'] = $req_user['password'];
+            header('Location: index.php');
+        }
+        else {
+            $error_signin = "Wrong Ids";
+        }
+    }
+    else {
+        $error_signin = "Missing information for signup";
+    }
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -11,7 +34,20 @@
     <title>ezratp</title>
 </head>
 <body>
-<?php include 'header.php'; ?>
+
+<form method="POST" action="">
+    <input type="email" name="emailconnect" placeholder="email" />
+    <input type="password" name="passwordconnect" placeholder="password" />
+    <p style="color: red"><?php
+        if(isset($error_signin)){
+            echo $error_signin;
+        }
+        ?></p>
+    <input name="formsignin" type="submit" value="Sign in">
+</form>
+
+
+<?php //include 'header.php'; ?>
 <h1>Check up a station :</h1>
 <form action="/stations.php">
     Station Name:<br>
