d76c28c6b6486d69eeb1d41245c6e363a878257f
Amine <sghir.ma@gmail.com>
2018-08-28 00:07:07 +0200

    update

--- header.php
+++ header.php
@@ -5,18 +5,16 @@ $db = new PDO('mysql:host=localhost;dbname=ezratp', 'root', 'VnCdE28u');
 $connected = false;
 
 if(isset($_SESSION['email']) AND isset($_SESSION['password'])){
-    if(!empty($emailconnect) AND !empty($passwordconnect)) {
-        $req_user = $db->prepare("SELECT * FROM users WHERE email = ? AND password = ?");
-        $req_user->execute(array($_SESSION['email'], $_SESSION['password']));
-        $correct_ids = $req_user->rowCount();
-        if ($correct_ids == 1) {
-            $user_info = $req_user->fetch();
-            $connected = true;
-            $_SESSION['id'] = $user_info['id'];
-            $_SESSION['username'] = $user_info['username'];
-            $_SESSION['email'] = $user_info['email'];
-            $_SESSION['password'] = $user_info['password'];
-        }
+    $req_user = $db->prepare("SELECT * FROM users WHERE email = ? AND password = ?");
+    $req_user->execute(array($_SESSION['email'], $_SESSION['password']));
+    $correct_ids = $req_user->rowCount();
+    if ($correct_ids == 1) {
+        $user_info = $req_user->fetch();
+        $connected = true;
+        $_SESSION['id'] = $user_info['id'];
+        $_SESSION['username'] = $user_info['username'];
+        $_SESSION['email'] = $user_info['email'];
+        $_SESSION['password'] = $user_info['password'];
     }
 }
 
