0d8c42870a102d62f0497ee8cc3ba6af3bbdf3b9
Amine Sghir <amine.sghir-ext@lyxor.com>
2018-09-20 15:31:37 +0200

    update

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="index.php"><img style="height: 300%;" src="assets/ezratp/logo.png" alt="logo"></a>
+            <a class="navbar-brand" href="index.php"><img style="height: 250%;" src="assets/ezratp/logo.png" alt="logo"></a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
