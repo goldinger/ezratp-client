f9569b11fd98482ce886cdbd0927a893c8966428
Amine Sghir <amine.sghir-ext@lyxor.com>
2018-09-20 15:29:26 +0200

    update

--- slider.php
+++ slider.php
@@ -1,9 +1,9 @@
 <div class="slider">
     <div id="bg-slider" class="owl-carousel owl-theme">
 
-        <div class="item"><img src="assets/ezratp/slider1.jpg" alt="Bus RATP"></div>
-        <div class="item"><img src="assets/ezratp/slider2.jpg" alt="Metro"></div>
-        <div class="item"><img src="assets/ezratp/slider3.jpg" alt="Tramway"></div>
+        <div class="item"><img src="assets/ezratp/slider1.JPG" alt="Bus RATP"></div>
+        <div class="item"><img src="assets/ezratp/slider2.JPG" alt="Metro"></div>
+        <div class="item"><img src="assets/ezratp/slider3.JPG" alt="Tramway"></div>
 
     </div>
 </div>
